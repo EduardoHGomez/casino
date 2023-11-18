16b79e691a07c1a82c77d49c73f07f3aaddda31f
David Hernández <david.hernandezc@iteso.mx>
2023-11-17 22:28:51 -0600

    connection

--- src/controllers/login_utils.js
+++ src/controllers/login_utils.js
@@ -1,3 +1,11 @@
-const mongoose = require('../../routes/mongoose');
 
-const emailLogin = document.getElementById('email')
+
+const emailLogin = document.getElementById('email');
+const passLogin = document.getElementById('password');
+const actionLogin = document.getElementById('profiteering');
+
+actionLogin.addEventListener('click', () =>
+{
+    console.log(emailLogin.value);
+    console.log(passLogin.value)
+})
