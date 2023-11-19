b9c9d1db4129b8a9c66568371409afe90390769e
David Hernández <david.hernandezc@iteso.mx>
2023-11-19 16:44:09 -0600

    updating the roulette logic

--- src/controllers/login_connect.js
+++ src/controllers/login_connect.js
@@ -3,6 +3,10 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 
+/*
+
+Esquema sin requerimientos
+
 let expectedSchema = mongoose.Schema({
     name: String,
     age: Number,
@@ -10,6 +14,32 @@ let expectedSchema = mongoose.Schema({
     email: String,
     password: String
 });
+ */
+
+let expectedSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 18,
+        max: 99,
+    },
+    balance: {
+        type: Number,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
 
 
 let User = mongoose.model('users',expectedSchema);
