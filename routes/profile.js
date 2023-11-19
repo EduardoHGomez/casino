4f3e487f1c58788c3f051db1ebfd2214d7c8ce0e
EduardoHGomez <hurtado.josegomez@gmail.com>
2023-11-18 23:51:55 -0600

    Balance Add Method

--- routes/profile.js
+++ routes/profile.js
@@ -2,20 +2,38 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// /profile/
+// --------------- GET METHODS -----------------------
+
+// ----- Profile ---------
+
+// /profile/ => load HTML
 router.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/../src/views/profile.html"));
 });
 
-// /profile/activity
-router.get("/activity", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "/../src/views/activity.html"));
-});
+// /profile/user => load current user's info
+
+
+// ----- Balance ---------
 
 // /profile/balance
 router.get("/balance", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/../src/views/balance.html"));
 });
 
+// Update balance (add or withdraw)
+router.put("/balance", (req, res) => {
+
+    // Find Id from User
+
+    res.send('Hola');
+
+});
+
+// ----- Activity ---------
+// /profile/activity
+router.get("/activity", (req, res) => {
+    res.sendFile(path.resolve(__dirname + "/../src/views/activity.html"));
+});
 
 module.exports = router;
---
+++ src/scripts/balance.js
@@ -0,0 +1,19 @@
+const xhr = new XMLHttpRequest();
+
+function addBalance() {
+    let amount = document.querySelector('#balance-form-deposit').value;
+    amount = parseFloat(amount);
+
+    xhr.open('GET', `/balance`, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            alert(xhr.status + ': ' + xhr.statusText); 
+        } else { 
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        }
+    };
+    xhr.send();
+}
