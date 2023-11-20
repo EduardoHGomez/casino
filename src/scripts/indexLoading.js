f6a0b9968af00c745d7ac685f8ef6af13943c2b7
David Hernández <david.hernandezc@iteso.mx>
2023-11-19 19:27:30 -0600

    fixed the errores from the html

--- src/scripts/indexLoading.js
+++ src/scripts/indexLoading.js
@@ -1,5 +1,4 @@
-const next = document.getElementById('next');
-const prev = document.getElementById('prev')
+
 const text = document.getElementById('textProf')
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -36,24 +35,6 @@ function loadName()
 }
 
 let defaultTransform = 0;
-function goNext()
-{
-    defaultTransform = defaultTransform - 398;
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-
-next.addEventListener('click', goNext);
-
-function goPrev()
-{
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + 398;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-prev.addEventListener("click", goPrev);
 
 
 function goToIndex()
