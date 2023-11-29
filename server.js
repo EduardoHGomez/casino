92421ed65613364108c75f9933aebe0e5ad06202
David Hernández <david.hernandezc@iteso.mx>
2023-11-29 08:31:39 -0600

    asd

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Swal = require('sweetalert2')
 
 // ---------- CONFIGURACIÓN DEL SERVIDOR ------------ Ejecutar con npm run dev
 const app = express(); 
-const port = 3000;
+const port = process.env.PORT ||  3000;
 app.use(express.json())
 
 
